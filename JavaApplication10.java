af305df8d7109c40d086ec731053a2b3bb28b23b
gerasantillan17 <gerapumas122@gmail.com>
2024-09-09 10:53:50 -0600

    Add files via upload

---
+++ JavaApplication10.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication10;
+
+/**
+ *
+ * @author gerar
+ */
+public class JavaApplication10 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 5;
+        
+        int suma, resta, divi, multi, pot, raiz;
+        suma= x + y;
+        System.out.println("El resultado de la suma es: ");
+        System.out.println(suma);
+        
+        resta= x - y;
+        System.out.println("El resultado de la resta es: ");
+        System.out.println(resta);
+        
+        multi= x * y;
+        System.out.println("El resultado de la ultiplicacion: ");
+        System.out.println(multi);
+        
+        divi= x / y;
+        System.out.println("El resultado de dividir es: ");
+        System.out.println(divi);
+        
+        pot= x*x;
+        System.out.println("El resultado es: ");
+        System.out.println(pot);
+        
+        pot= y*y;
+        System.out.println("El resultado es: ");
+        System.out.println(pot);
+        
+        
+   }
+    
+}
